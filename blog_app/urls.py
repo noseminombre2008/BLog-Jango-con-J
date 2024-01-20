c67db09e2cfca2057e2c7b84ac6d628231477115
Santiago Martinez <peladio98@gmail.com>
2024-01-20 11:39:01 -0400

    create read

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, BlogDetailView, BlogCreateView
 
 urlpatterns=[
     path("", HomePageView.as_view(), name="home"),
-    path("about/", AboutPageView.as_view(), name="about")
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("post/<int:pk>", BlogDetailView.as_view(), name='post_detail'),
+    path("post/new", BlogCreateView.as_view(), name="post_new"),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,19 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView
+from .models import Post
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model= Post  
     template_name="home.html"
 
 class AboutPageView(TemplateView):
     template_name="about.html"
+
+class BlogDetailView(DetailView):
+    model=Post
+    template_name="post_detail.html"
+
+class BlogCreateView(CreateView):
+    model=Post
+    template_name="post_new.html"
+    fields= ["title", "author", "body"]
