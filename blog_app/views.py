d13d5add34bebadb718a928d9a2947331efdac04
Santiago Martinez <peladio98@gmail.com>
2024-01-27 10:18:44 -0400

    update

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView, ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
 
 class HomePageView(ListView):
@@ -17,3 +17,9 @@ class BlogCreateView(CreateView):
     model=Post
     template_name="post_new.html"
     fields= ["title", "author", "body"]
+
+class BlogUpdateView(UpdateView):
+    model=Post
+    template_name="post_edit.html"
+    fields=["title", "body"]
+
